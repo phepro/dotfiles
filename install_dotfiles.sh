1ec2039a26914cd9b284f1f99715755cb9f27856
crocodile_lover <158551266+phepro@users.noreply.github.com>
2026-08-30 18:56:01 +0200

    Create install_dotfiles.sh

---
+++ install_dotfiles.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+git clone https://github.com/phepro/dotfiles.git
+
+sudo pacman -S fish kitty mako neovim rofi sway swaylock waybar yazi stow
+
+sudo rm -r ~/.config/kitty/
+sudo rm -r ~/.config/fish/
+sudo rm -r ~/.config/nvim/
+sudo rm -r ~/.config/yazi/
+sudo rm -r ~/.config/waybar/
+sudo rm -r ~/.config/sway/
+sudo rm -r ~/.config/rofi/
+sudo rm -r ~/.config/mako/
+sudo rm -r ~/.swaylock/
+
+
