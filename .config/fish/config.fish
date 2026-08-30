faebb4e1f977b55139d198e0494e9069bc14635d
crocodile_lover <158551266+phepro@users.noreply.github.com>
2026-08-30 19:16:11 +0200

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,3 @@
-alias hypr-config='nvim .config/hypr/hyprland.lua'
-alias hypr-config-kb='nvim .config/hypr/modules/keybinds.lua'
-alias hypr-config-as='nvim .config/hypr/modules/autostart.lua'
-alias hypr-config-deco='nvim .config/hypr/modules/decorations.lua'
-
 alias sway-config='nvim .config/sway/config'
 
 alias wayb-config='nvim .config/waybar/config.jsonc'
--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -6,14 +6,14 @@ cd
 
 sudo pacman -S fish kitty mako neovim rofi sway swaylock waybar yazi stow
 
-sudo rm -r ~/.config/kitty/
-sudo rm -r ~/.config/fish/
-sudo rm -r ~/.config/nvim/
-sudo rm -r ~/.config/yazi/
-sudo rm -r ~/.config/waybar/
-sudo rm -r ~/.config/sway/
-sudo rm -r ~/.config/rofi/
-sudo rm -r ~/.config/mako/
-sudo rm -r ~/.swaylock/
+# sudo rm -r ~/.config/kitty/
+# sudo rm -r ~/.config/fish/
+# sudo rm -r ~/.config/nvim/
+# sudo rm -r ~/.config/yazi/
+# sudo rm -r ~/.config/waybar/
+# sudo rm -r ~/.config/sway/
+# sudo rm -r ~/.config/rofi/
+# sudo rm -r ~/.config/mako/
+# sudo rm -r ~/.swaylock/
 
 stow -d ~/dotfiles -t ~/ .
