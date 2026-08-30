3055edba0fc12fd51ea4c6e0c2c75a3606196c9c
crocodile_lover <158551266+phepro@users.noreply.github.com>
2026-08-30 18:42:11 +0200

    everything

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+alias hypr-config='nvim .config/hypr/hyprland.lua'
+alias hypr-config-kb='nvim .config/hypr/modules/keybinds.lua'
+alias hypr-config-as='nvim .config/hypr/modules/autostart.lua'
+alias hypr-config-deco='nvim .config/hypr/modules/decorations.lua'
+
+alias sway-config='nvim .config/sway/config'
+
+alias wayb-config='nvim .config/waybar/config.jsonc'
+alias wayb-style='nvim .config/waybar/style.css'
+alias fish-config='nvim .config/fish/config.fish'
+
+alias calc_app='./Projects/CalcApp/calc'
+
+# macchina -t Hydrogen
+fastfetch
